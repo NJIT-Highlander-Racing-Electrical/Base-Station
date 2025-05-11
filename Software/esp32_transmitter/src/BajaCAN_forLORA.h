a4064d2472a203afad65638fb80374305bc67bb3
andrewsantella <72953033+andrewsantella@users.noreply.github.com>
2025-05-10 20:36:18 -0400

    Reverting to 500kbps

--- Software/esp32_transmitter/src/BajaCAN_forLORA.h
+++ Software/esp32_transmitter/src/BajaCAN_forLORA.h
@@ -424,7 +424,7 @@ void setupCAN()
   memset(&all_data, 0, sizeof(all_data)); // initalize data variable to 0
 
   twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_1, GPIO_NUM_38, TWAI_MODE_NORMAL);
-  twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();
+  twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
   twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
   if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK)
